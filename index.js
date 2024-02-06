251dc431aab211e28baac44812fa8793234c116b
STXD6 <159173562+STXD6@users.noreply.github.com>
2024-02-06 20:03:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Где то метаеться по зоне","Ходит лутает локаций","Где то метаеться по зоне"];
 
 
 let currentIndex = 0;
